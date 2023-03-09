147fd9c55cd4dd74803f263ad998f46ffe152584
Kazeem Odutola <kazeemodutola@pc-129-60.customer.ask4.lan>
2023-03-09 12:07:19 +0000

    chore: add a base model and base repository

---
+++ src/data/models/base.model.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreatedAt,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt
+} from "sequelize-typescript";
+
+@Table
+export class BaseModel extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @CreatedAt
+  @Column({ allowNull: false })
+  createdAt!: Date;
+
+  @UpdatedAt
+  @Column({ allowNull: false })
+  updatedAt!: Date;
+}
---
+++ src/data/repositories/base.repo.ts
@@ -0,0 +1,52 @@
+import { Model } from "sequelize-typescript";
+
+export interface IBaseRepository {
+  all(attributes?: any): Promise<any | any[]>;
+  findOne(attributes?: any): Promise<any>;
+  findById(id: string, attributes?: any): Promise<any>;
+  create(data: any, transaction?: any): Promise<any>;
+  updateRows(id: string, data: any): Promise<any>;
+  deleteRow(id: string): Promise<number>;
+}
+
+// @TODO: Remove //@ts-ignore, make Model compile with types
+export abstract class BaseRepository<T extends Model> implements IBaseRepository {
+  constructor(protected model: typeof Model) {}
+
+  async all(attributes?: any): Promise<T | T[]> {
+    // @ts-ignore
+    return this.model.findAll({
+      where: { ...attributes }
+    });
+  }
+
+  async findById(id: string, attributes?: any) {
+    // @ts-ignore
+    return await this.model.findByPk(id, {
+      attributes
+    });
+  }
+
+  async create(data: any, t?: any): Promise<T> {
+    // @ts-ignore
+    return this.model.create(data, { transaction: t });
+  }
+
+  async updateRows(id: string, data: any): Promise<T> {
+    const resource = await this.findById(id);
+
+    if (resource) {
+      // @ts-ignore
+      return await resource.update(data)
+    }
+  }
+
+  async deleteRow(id: string): Promise<number> {
+    return await this.model.destroy({ where: { id } });
+  }
+
+  async findOne([T]: any) {
+    // @ts-ignore
+    return await this.model.findOne({ ...T });
+  }
+}
