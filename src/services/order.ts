f1d6dad2e65f0f1696d8af6f3a4d88b3cecd3a0c
tobslob <kazmobileapp@gmail.com>
2023-03-10 00:26:00 +0000

    add order services

---
+++ src/services/order.ts
@@ -0,0 +1,32 @@
+import { OrderDTO, QueryDTO } from "@app/data/models";
+import { OrderRepo } from "@app/data/repositories/order.repo";
+import { Request } from "express";
+
+class OrderService {
+  async createOrder(order: OrderDTO, req: Request) {
+    order["organizationId"] = req["user"].organizationId
+
+    return await OrderRepo.create(order)
+  }
+
+
+
+
+  async getOrderById(id: string) {
+    return (await OrderRepo.findById(id))?.toJSON();
+  }
+
+  async getOrders(query: QueryDTO) {
+    return await OrderRepo.all(query);
+  }
+
+  async deleteOrder(id: string) {
+    return await OrderRepo.deleteRow(id);
+  }
+
+  async updateOrder(id: string, order: OrderDTO) {
+    return await OrderRepo.updateRows(id, order);
+  }
+}
+
+export const Orders = new OrderService();
