c316db614cf0a8a01046f7611c33fb6ae849bf94
tobslob <kazmobileapp@gmail.com>
2023-03-10 19:37:30 +0000

    feat: add create order service

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,28 +1,66 @@
-import { OrderDTO, QueryDTO } from "@app/data/models";
-import { OrderRepo } from "@app/data/repositories/order.repo";
+import db from "@app/data/database/connect";
+import { OrderDTO, OrderProduct, QueryDTO } from "@app/data/models";
+import { orderRepo } from "@app/data/repositories/order.repo";
+import { orderProductRepo } from "@app/data/repositories/orderProduct.repo";
+import { productRepo } from "@app/data/repositories/product.repo";
 import { Request } from "express";
 
 class OrderService {
   async createOrder(order: OrderDTO, req: Request) {
     order["organizationId"] = req["user"].organizationId;
+    let createdOrder;
+    let orderProduct: OrderProduct[] = [];
 
-    return await OrderRepo.create(order);
+    await db.sequelize.transaction().then(async t => {
+      createdOrder = (await orderRepo.create(order, { ...t })).toJSON();
+
+      for (const product of order.products) {
+        const getProduct = (await productRepo.findById(product.productId, t)).toJSON();
+
+        const orderProd = await orderProductRepo.create(
+          {
+            orderId: createdOrder.id,
+            // @ts-ignore
+            productId: getProduct?.id,
+            volume: product.volume,
+            // @ts-ignore
+            pricePerUnit: getProduct?.pricePerUnit
+          },
+          t
+        );
+
+        orderProduct.push(orderProd);
+
+        // @ts-ignore
+        const volume = getProduct?.volume - product.volume;
+
+        await productRepo.updateRows(
+          product.productId,
+          {
+            volume
+          },
+          { transaction: t }
+        );
+      }
+    });
+
+    return { ...createdOrder, orderProduct };
   }
 
-  async getOrderById(id: string) {
-    return (await OrderRepo.findById(id))?.toJSON();
+  async getOrderById(id: string, t?: any) {
+    return (await orderRepo.findById(id, t))?.toJSON();
   }
 
   async getOrders(query: QueryDTO) {
-    return await OrderRepo.all(query);
+    return await orderRepo.all(query);
   }
 
   async deleteOrder(id: string) {
-    return await OrderRepo.deleteRow(id);
+    return await orderRepo.deleteRow(id);
   }
 
   async updateOrder(id: string, order: OrderDTO) {
-    return await OrderRepo.updateRows(id, order);
+    return await orderRepo.updateRows(id, order);
   }
 }
 
